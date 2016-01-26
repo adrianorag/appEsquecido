8eca8f4e5f25272ebd3747b478418ef4a8042e18
Adriano Gomes <adrianorag@gmail.com>
2016-01-26 18:53:38 -0200

    Ajuste no service

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope, $cordovaLocalNotification, $ionicPlatform) {
+.controller('DashCtrl', function($scope,$cordovaLocalNotification, $ionicPlatform,NotificationFactory,ContadorFactory) {
       $scope.alerta ={
         tempoAlerta : 0,
         tituloAlerta : ""
@@ -10,7 +10,6 @@ angular.module('starter.controllers', [])
         $scope.alerta.tempoAlerta += tempoMin;
       }
 
-
       $scope.resetForm = function(){
         $scope.alerta.tempoAlerta = 0;
         $scope.alerta.tituloAlerta= '';
@@ -18,23 +17,12 @@ angular.module('starter.controllers', [])
 
       $ionicPlatform.ready(function () {
         $scope.createSingleNotification = function () {
-
-          var listNotification = window.localStorage.getItem("listNotification");
-          var contador = window.localStorage.getItem("contador");
-          contador = contador == null ? 1 : parseInt(contador);
-          listNotification = listNotification == null ? [] : JSON.parse(listNotification);
-
           var now = new Date().getTime();
           var tempoMin = $scope.alerta.tempoAlerta * 60;
           var _timeAlert = new Date(now + $scope.alerta.tempoAlerta * 60 * 1000);
-          var _textAlert =  $scope.alerta.tituloAlerta;
 
-          var newNotification = {title: 'Lembre-se', id: contador, text:_textAlert,  at: _timeAlert, tempoMin: tempoMin };
-          listNotification.push(newNotification);
-
-          contador ++;
-          window.localStorage.setItem("listNotification",JSON.stringify(listNotification));
-          window.localStorage.setItem("contador",contador);
+          var newNotification = {title: 'Lembre-se', id: ContadorFactory.get(), text: $scope.alerta.tituloAlerta,  at: _timeAlert, tempoMin: tempoMin };
+          NotificationFactory.add(newNotification);
 
           $cordovaLocalNotification.schedule({
             id:newNotification.id,
@@ -48,34 +36,27 @@ angular.module('starter.controllers', [])
           $scope.resetForm();
         };
 
-        $scope.cancelSingleNotification = function () {
-          $cordovaLocalNotification.cancel(3).then(function (result) {
-            console.log('Notification 3 Canceled');
-          });
-        };
       });
 })
 
-.controller('ListCtrl', function($scope, $cordovaLocalNotification) {
-  var listNotification = window.localStorage.getItem("listNotification");
-  listNotification = listNotification == null ? [] : JSON.parse(listNotification);
-
-  var dataAtual = Date();
-  angular.forEach(listNotification, function(value, key) {
-      value.dataPassada =new Date(dataAtual).getTime()> new Date(value.at).getTime();
-  }, this);
-
-  $scope.listNotification= listNotification;
-
+.controller('ListCtrl', function($scope, $cordovaLocalNotification,NotificationFactory) {
   $scope.cancelNotificantion = function(itemNotification){
     console.log(itemNotification.id);
     $cordovaLocalNotification.cancel(itemNotification.id);
-    var lista = $scope.listNotification;
-    angular.forEach($scope.listNotification, function (item,i){
-      if(item.id == itemNotification.id){
-        $scope.listNotification.splice(i, 1);
-      }
-    });
+    NotificationFactory.remove(itemNotification);
+    $scope.refreshListNotification();
   };
 
+  $scope.refreshListNotification = function(){
+    var listNotification = NotificationFactory.all();
+    var dataAtual = Date();
+    angular.forEach(listNotification, function(obj, key) {
+        obj.dataPassada =new Date(dataAtual).getTime()> new Date(obj.at).getTime();
+        
+    }, this);
+
+    $scope.listNotification= listNotification;
+  }
+
+  $scope.refreshListNotification();
 });
--- www/js/services.js
+++ www/js/services.js
@@ -1,6 +1,7 @@
 angular.module('starter.services', [])
 
-.factory('Chats', function() {
+.factory('NotificationFactory', function() {
+
   var listNotification = window.localStorage.getItem("listNotification");
   listNotification = listNotification == null ? [] : JSON.parse(listNotification);
 
@@ -8,28 +9,41 @@ angular.module('starter.services', [])
     all: function(){
       return listNotification;
     },
+    get: function(id) {
+      for (var i = 0; i < listNotification.length; i++) {
+        if (listNotification[i].id === parseInt(id)) {
+          return listNotification[i];
+        }
+      }
+      return null;
+    },
     add:function(newNotification){
       listNotification.push(newNotification);
       window.localStorage.setItem("listNotification",JSON.stringify(listNotification));
+    },
+    remove:function(notification){
+      angular.forEach(listNotification, function (item,i){
+        if(item.id == notification.id){
+          listNotification.splice(i, 1);
+          window.localStorage.setItem("listNotification",JSON.stringify(listNotification));
+          return true;
+        }
+      });
+      return false;
     }
-
-
   };
 })
-.factory('contador', function() {
-  var listNotification = window.localStorage.getItem("listNotification");
-  listNotification = listNotification == null ? [] : JSON.parse(listNotification);
+
+.factory('ContadorFactory', function() {
+  var contador = window.localStorage.getItem("contador");
+  this.contador = this.contador == null ? 1 : parseInt(this.contador);
 
   return {
-    all: function(){
-      return listNotification;
-    },
-    add:function(newNotification){
-      listNotification.push(newNotification);
-      window.localStorage.setItem("listNotification",JSON.stringify(listNotification));
+    get: function(){
+      contador++;
+      window.localStorage.setItem("contador",this.contador);
+      return this.contador;
     }
-
-
   };
 })
 ;
